b097f179d33f9c5094dc3b7d7665904458837b65
cherryvue <yetiasg@gmail.com>
2021-12-02 08:42:37 +0100

    change port

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,4 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
-app.listen(3005, () => console.log('Listening on port 3005'))
+app.listen(3004, () => console.log('Listening on port 3004'))
